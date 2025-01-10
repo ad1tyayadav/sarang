f153ffced54e83a9e778b90c002b031a1db41440
Adii Code <adityaproworks@gmail.com>
2025-01-10 19:10:41 +0530

    Change

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/', (req, res) => {
 });
 
 // Set up server to listen on the defined port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
